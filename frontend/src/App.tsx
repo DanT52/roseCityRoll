425f4f13796c16955811e0507c757f7aaf7a862e
Daniel Basarab <dantramp52@gmail.com>
2025-01-21 14:59:24 -0800

    add hashrouter to home faq and schedule pages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
+import FAQPage from './pages/FAQ';
+import SchedulePage from './pages/Schedule';
 
 function App() {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/faq" element={<FAQPage />} />
+          <Route path="/schedule" element={<SchedulePage />} />
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
