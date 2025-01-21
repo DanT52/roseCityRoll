f4f3e7cfa5bcf54204c7462e870a1d5f563214c3
Daniel Basarab <dantramp52@gmail.com>
2025-01-21 14:59:05 -0800

    update config for hosting

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Tell Vite that the site will be served from /roseCityRoll/ on GitHub Pages
-  base: '/roseCityRoll/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
