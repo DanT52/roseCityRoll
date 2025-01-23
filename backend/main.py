a6a3dbdc9e54e5c7d1ff26f32e55fafab0912eb2
DanT52 <dantramp52@gmail.com>
2025-01-23 18:27:27 +0000

    enable reload

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,9 @@
+"""
+This module runs the uvicorn server for the FastAPI application.
+"""
+
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", host="0.0.0.0", log_level="info")
+    uvicorn.run("app.app:app", host="0.0.0.0", log_level="info", reload=True)
+    
