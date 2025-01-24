0577ca6e41f3b47f892709455c517d31c3d33a7a
Daniel Basarab <dantramp52@gmail.com>
2025-01-24 15:04:20 -0800

    config

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
 """
 This module runs the uvicorn server for the FastAPI application.
 """
-
+import argparse
 import uvicorn
 
+
 if __name__ == "__main__":
-    uvicorn.run("app.app:app", host="0.0.0.0", log_level="info", reload=True)
+    parser = argparse.ArgumentParser(description="Run the FastAPI application with Uvicorn.")
+    parser.add_argument("--reload", action="store_true", help="Enable auto-reload.")
+    args = parser.parse_args()
+
+    uvicorn.run("app.app:app", host="0.0.0.0", log_level="info", reload=args.reload)
     
