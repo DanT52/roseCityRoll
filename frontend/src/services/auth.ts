a8620b06d85d5fdecb505af48e2bb12adcec15d1
Daniel Basarab <dantramp52@gmail.com>
2025-01-24 22:07:01 +0000

    fix auth url

--- frontend/src/services/auth.ts
+++ frontend/src/services/auth.ts
@@ -1,7 +1,7 @@
 const BACKEND_URL = import.meta.env.VITE_REACT_APP_BACKEND_URL || 'http://localhost:8000';
 
 export const login = async (username: string, password: string) => {
-  const res = await fetch(BACKEND_URL, {
+  const res = await fetch(BACKEND_URL + "/auth/jwt/login", {
     method: 'POST',
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: new URLSearchParams({ username, password })
