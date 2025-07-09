80e715fc02ee3325a7a0c96801f3b35cb87990f4
Daniel Basarab <dantramp52@gmail.com>
2025-07-09 15:26:45 -0700

    add google tracking

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import { HashRouter as Router, Routes, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import FAQPage from './pages/FAQ';
 import SchedulePage from './pages/Schedule';
 import Thanks from './pages/Thanks';
 
+// Google Analytics tracking hook
+function usePageTracking() {
+  const location = useLocation();
+  useEffect(() => {
+    if (window.gtag) {
+      window.gtag('config', 'G-4K2ENH6D1P', {
+        page_path: location.pathname,
+      });
+    }
+  }, [location]);
+}
+
+function PageTracker() {
+  usePageTracking();
+  return null;
+}
+
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
+      <PageTracker />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
